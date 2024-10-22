f280ad3b14728312465ccd5b48706c997757c436
MarcosDev001 <109878361+marcosboni7@users.noreply.github.com>
2024-10-22 12:05:09 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,15 +1,13 @@
 const express = require('express');
-const bodyParser = require('body-parser');
 const session = require('express-session');
 const flash = require('connect-flash');
-const userRoutes = require('./routes/index');
-const path = require('path');
+const userRoutes = require('../routes/index');
 
 const app = express();
 
-// Configurações do middleware
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
+// Middleware
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
 app.use(session({
     secret: 'seuSegredoAqui',
     resave: false,
@@ -17,20 +15,8 @@ app.use(session({
 }));
 app.use(flash());
 
-// Configurando EJS como engine de visualização
-app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views'));
-
-// Serve arquivos estáticos
-app.use(express.static(path.join(__dirname, 'public')));
-
-// Rota principal
-app.get('/', (req, res) => {
-    res.redirect('/login'); // Redireciona para a página de login
-});
-
 // Rotas
 app.use('/', userRoutes);
 
-// Exporta o aplicativo para que a Vercel possa gerenciá-lo
+// Exporta para Vercel
 module.exports = app;

d1f600947d40f95100f1ce06e3e0507589651b76
MarcosDev001 <109878361+marcosboni7@users.noreply.github.com>
2024-10-21 21:55:57 -0300

    Update app.js

--- app.js
+++ app.js
@@ -24,7 +24,16 @@ app.set('views', path.join(__dirname, 'views'));
 // Serve arquivos estáticos
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Rota principal
+app.get('/', (req, res) => {
+    res.redirect('/login'); // Redireciona para a página de login
+});
+
 // Rotas
 app.use('/', userRoutes);
 
 // O Vercel gerenciará a inicialização do servidor
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Servidor rodando na porta ${PORT}`);
+});

81804c6dbd8271c193d78d81ed1d180f5c0e3359
MarcosDev001 <109878361+marcosboni7@users.noreply.github.com>
2024-10-22 10:00:18 -0300

    Update db.js

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ const { Pool } = require('pg');
 
 const pool = new Pool({
     user: process.env.DB_USER || 'admin',
-    host: process.env.DB_HOST || 'localhost',
+    host: process.env.DB_HOST || 'https://xmania-five.vercel.app/',
     database: process.env.DB_NAME || 'xmania',
     password: process.env.DB_PASSWORD || 'admin',
     port: process.env.DB_PORT || 5432,

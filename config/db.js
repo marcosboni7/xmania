f2f64402065368903dd965548cc0d80fbbfc24ac
MarcosDev001 <109878361+marcosboni7@users.noreply.github.com>
2024-10-22 09:41:35 -0300

    db

--- config/db.js
+++ config/db.js
@@ -1,12 +1,11 @@
- 
 const { Pool } = require('pg');
 
 const pool = new Pool({
-    user: 'admin',
-    host: 'localhost',
-    database: 'xmania',
-    password: 'admin',
-    port: 5432,
+    user: process.env.DB_USER || 'admin',
+    host: process.env.DB_HOST || 'localhost',
+    database: process.env.DB_NAME || 'xmania',
+    password: process.env.DB_PASSWORD || 'admin',
+    port: process.env.DB_PORT || 5432,
 });
 
 module.exports = {
